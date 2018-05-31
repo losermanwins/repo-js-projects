dad8a96a075655da48bd616643ab65304e4e13c1
Jack Andersen <jandersen101@gmail.com>
2018-05-31 10:08:41 -0500

    Extended to include palindrome checker

--- Practice JS Projects/Reverse String/app.js
+++ Practice JS Projects/Reverse String/app.js
@@ -1,3 +1,5 @@
+//EXTENDED : Includes Reverse String and Palindrome Check
+
 //Take in a string from the user (figure out how to take input) //works
 let input = prompt("Please enter a word");
 document.writeln(input);
@@ -8,3 +10,9 @@ for (i = 0; i < input.length; i++){
 }
 let rword = reverse.join("");
 document.writeln(rword);
+if (rword === input) {
+    document.writeln("This is a palindrome!");
+}
+else {
+    document.writeln("This is not a palindrome :( ");
+}
