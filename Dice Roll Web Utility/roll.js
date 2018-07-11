031d9cf51191e01d8fc5fdf409293e0fba305f7a
Jack Andersen <jandersen101@gmail.com>
2018-07-11 15:42:54 -0500

    Updated Dice Roll utility to work using nodejs and take user input

--- Dice Roll Web Utility/roll.js
+++ Dice Roll Web Utility/roll.js
@@ -1,6 +1,23 @@
 //function that takes 2 inputs, one for number of dice and one for sides
 //result should be a sum adding up each dice, a random number ranging from 1 to n sides
 //find a way to add html functionality of buttons?
+var readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+    var a = 0;
+    var b = 0;
+    rl.question("How many sides does the dice have?", (answer1) => {
+        a = answer1;
+        rl.question("How many times do you want to roll the dice?", (answer2) => {
+        b = answer2;
+        diceRoll(b, a);
+        rl.close();
+        });
+    });
+   
 
 function diceRoll (n, sides) {
     let rollTotal = 0;
@@ -8,8 +25,8 @@ function diceRoll (n, sides) {
         let nextRoll = Math.floor((Math.random() * sides) + 1); 
         console.log("You rolled : " + nextRoll);
         rollTotal += nextRoll;
-        console.log(rollTotal);
+        console.log("Total : " + rollTotal);
     }
 }
 
-diceRoll(document.getElementsByName("noDice"),document.getElementsByName("Sides"));
+diceRoll();
